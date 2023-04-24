f6641b7a61a242a2c377a42e66fea7df57de9c8a
Torge <torge.rau@gmx.de>
2023-04-24 15:45:09 +0200

    Hashende Mange realisation angefangen

---
+++ include/hashendeMengerealisation.hpp
@@ -0,0 +1,22 @@
+#pragma once 
+
+#include <iostream>
+#include "../include/hashendemenge.hpp"
+
+namespace ProjectAlpha
+{
+
+    template<class T>
+    class hashendemengerela : public hashendemenge<T>{
+        public:
+            //hier folgen public Funktionen der Hashtabelle
+
+        private:
+            //hier folgen private Funktionen der Hashtabelle    
+
+    };
+
+    
+
+
+}
