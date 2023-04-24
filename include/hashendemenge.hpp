5e970a65a6194f8bdbf656adaf53c7bcc0d4fe47
Torge <torge.rau@gmx.de>
2023-04-24 14:30:45 +0200

    Kommentare zur hashenden Menge hinzugefuegt

--- include/hashendemenge.hpp
+++ include/hashendemenge.hpp
@@ -6,11 +6,11 @@ namespace ProjectAlpha
     class hashendemenge
     {
     public:
-        hashendemenge() = 0;
-        virtual void insert(T) = 0;
-        virtual bool find(T) = 0;
-        virtual void remove(T) = 0;
-        virtual void belegungsfaktor(T) = 0;
+        hashendemenge() = 0;                        //Konstruktor
+        virtual void insert(T) = 0;                 //Einfuegen eines Wertes
+        virtual bool find(T) = 0;                   //Kontrolle ob ein Wert bereits in der Hashtabelle enthalten ist
+        virtual void remove(T) = 0;                 //Entfernt einen Wert aus der Hashtabelle
+        virtual void belegungsfaktor(T) = 0;        //Belegungsfaktor ueberprueft ob die Hashtabelle vergroessert bzw. verkleinert werden soll
     };
 
 }
