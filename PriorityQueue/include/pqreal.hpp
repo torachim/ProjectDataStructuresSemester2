1fa43189d15d945f2d27663d106963b41b1c6586
Torge <torge.rau@gmx.de>
2023-04-29 15:35:05 +0200

    PQueue Header Datei fertig

--- PriorityQueue/include/pqreal.hpp
+++ PriorityQueue/include/pqreal.hpp
@@ -1,7 +1,33 @@
+#ifndef PQUEUE_REAL
+#define PQUEUE_REAL
 #include <iostream>
 #include "pqabstract.hpp"
 
+namespace ProjectAlpha{
+
+    template<class T>
+    class PQueue_realisation : public abstract_Pqueue<T> {
+    public:
+
+        PQueue_realisation();
+
+        size_t get_size();
+
+        void insert(T x);
+
+        T remove();
+
+        void print();
+
+    private:
+
+        size_t size;    
+
+    };
+}
 
 
 
+
+#endif
 //Hier folgt die realisation der Priority Queue
