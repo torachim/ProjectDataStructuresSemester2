cef1536f375b0bfb50f3075cbbd543e0aaafd150
snehpreet <dhinsasnehpreet@gmail.com>
2023-05-02 15:21:50 +0200

    Stack und Queue fertig

---
+++ Stack/examples/stackmain.cpp
@@ -0,0 +1,33 @@
+#include "../include/stack.hpp" 
+#include <iostream>
+
+
+
+
+using namespace ProjectAlpha;
+
+int main(){
+
+     stack<int>s;
+
+     s.push(6);
+     s.push(1);
+     s.push(9);
+     s.printStack();
+     s.pop();
+     s.printStack();
+     std::cout<< "Stack size:" << s.size() << std::endl;
+     
+
+
+}
+
+
+//clang++ -std=c++17 -I./include  examples/stackmain.cpp -o stack
+//./stack
+
+
+ 
+
+
+
