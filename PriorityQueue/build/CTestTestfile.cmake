99479c69454cd74c9f12d1fa38e14f568cbf283c
Torge <torge.rau@gmx.de>
2023-05-03 23:54:34 +0200

    Cmake zur PQ hinzugefuegt

---
+++ PriorityQueue/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/torgerau/Desktop/Uni Stuff/POOSE/projekt123sts/PriorityQueue
+# Build directory: /Users/torgerau/Desktop/Uni Stuff/POOSE/projekt123sts/PriorityQueue/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- PriorityQueue/examples/pqbsp.cpp
+++ PriorityQueue/examples/pqbsp.cpp
@@ -3,11 +3,12 @@
 #include<vector>
 
 
+
 using namespace ProjectAlpha;
 
 int main(){
     PQueue_realisation<int> pq = PQueue_realisation<int>();
-    pq.remove();
+    //pq.remove();
     pq.insert(5, 1);
     pq.insert(7, 5);
     pq.insert(100, 4);
