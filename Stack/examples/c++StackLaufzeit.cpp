9703a901a7390348d4ebfde6c91aeaf70dbbf8db
snehpreet <dhinsasnehpreet@gmail.com>
2023-05-06 22:45:27 +0200

    Laufzeiten für Stack bibothek erhoben

---
+++ Stack/examples/c++StackLaufzeit.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <chrono>
+
+int main(){
+
+     std::stack<int>s;
+     
+
+     // Messung der Zeit für das Pushen von 1000000 Elementen auf den Stack
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100000000; ++i)
+    {
+        s.push(i);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "Zeit für das Pushen: " << duration << " Mikrosekunden\n";
+
+    // Messung der Zeit für das Popping von 1000000 Elementen vom Stack
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100000000; ++i)
+    {
+        s.pop();
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "Zeit für das Popping: " << duration << " Mikrosekunden\n";
+
+    return 0;
+
+}
+
+
+//clang++ -std=c++17 -I./include  examples/c++StackLaufzeit.cpp -o std::stack
+//./std::stack
+
+
+ 
+
+
+
---
+++ Stack/examples/myStackLaufzeit.cpp
@@ -0,0 +1,37 @@
+#include "../include/stack.hpp" 
+#include <iostream>
+#include <chrono>
+
+
+using namespace ProjectAlpha;
+
+int main(){
+
+     stack<int>s;
+
+      // Messung der Zeit für das Pushen von x Elementen auf den Stack
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1000000; ++i)
+    {
+        s.push(i);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "Zeit für das Pushen von 1000000 Elementen: " << duration << " Mikrosekunden\n";
+
+    // Messung der Zeit für das Popping von x Elementen vom Stack
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1000000; ++i)
+    {
+        s.pop();
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "Zeit für das Popping von 1000000 Elementen: " << duration << " Mikrosekunden\n";
+
+    return 0;
+
+}
+
+
+//clang++ -std=c++17 -I./include  examples/myStackLaufzeit.cpp -o stack
